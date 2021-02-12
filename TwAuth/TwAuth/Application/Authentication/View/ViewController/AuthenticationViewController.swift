7532aca3ad84e12b1afa66961b2b55e3648a8d4f
Marlon David Ruiz Arroyave <Marlon.X.Ruiz.-ND@disney.com>
2021-02-12 11:06:59 -0500

    Organized project into MVVM Architecture.

---
+++ TwAuth/TwAuth/Application/Authentication/View/ViewController/AuthenticationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AuthenticationViewController.swift
+//  TwAuth
+//
+//  Created by Marlon David Ruiz Arroyave on 12/02/21.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
